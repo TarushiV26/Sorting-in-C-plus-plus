940435b8596ef7a216e11886e01b88b3ab3df4cc
Tarushi Verma <tarushiv2611@gmail.com>
2025-10-04 22:34:00 +0530

    Program2.cpp

---
+++ Program2.cpp
@@ -0,0 +1,31 @@
+//selection sort
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void selectionSort(std::vector<int>& arr){
+    int n = arr.size();
+    for(int i = 0; i < n - 1; i++){
+        int min_idx = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[min_idx]){
+                min_idx = j;
+            }
+        }
+        std::swap(arr[i], arr[min_idx]);
+    }
+}
+
+int main(){
+    std::vector<int> arr={18,27,8,-9,-1,5};
+    int n=arr.size();
+    selectionSort(arr);
+    std::cout<<"Sorted array: ";
+    for(int i=0;i<n;i++)
+        std::cout<<arr[i]<<" ";
+    std::cout<<std::endl;
+    return 0;
+}
+/*output:
+Sorted array: -9 -1 5 8 18 27 
+*/
